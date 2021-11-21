f1be60519636b6a6a0096599f85038c653540555
aravind <aravindsugunan@gmail.com>
2021-11-21 20:42:07 +0530

    Making Rate plots for Isolation Studies from L1Ntuples
    	modified:   Rate_ZeroBias.cc

--- Isolation/Rate_ZeroBias.cc
+++ Isolation/Rate_ZeroBias.cc
@@ -26,7 +26,7 @@
 #include "TLatex.h"
 #include "TMarker.h"
 #include "TAxis.h"
-
+#include "L1UpgradeTree.h"
 
 #define N_OPTIONS 23 // 1 [ no Iso ] + 22
 #define ET_MAX 255
@@ -36,6 +36,213 @@ TCanvas* CreateCanvas(TString CanvasName = "myPlot", bool LogY = false, bool Gri
 void DrawPrelimLabel(TCanvas* c);
 void DrawLumiLabel(TCanvas* c, TString Lumi = "35.9");
 void SaveCanvas(TCanvas* c, TString PlotName = "myPlotName");
+void tokenize(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters);
+void readLUTTable(std::string& file_name, unsigned int& nbin, std::map<unsigned int, unsigned int>& lut_map);
+
+void Rate_ZeroBias()
+{
+  
+   TFile f_Isolation("calibFiles/data2018_EGIsoV2_Options.root","READ");
+   TString FileName_in = "/grid_mnt/t3storage3/athachay/l1egamma/isolation/CMSSW_7_6_0/src/Isolation/slimL1EmulatorTrees/2018v3/Reduced_L1Ntuple_1.root";
+
+   std::map<TString,TH3F*> histosIsolation;
+    
+   TFile f_in(FileName_in.Data(),"READ");
+   TChain *treeChain = new TChain("L1UpgradeTree");
+   treeChain->Add(FileName_in);
+
+   cout << "Total Number of Events Available : " << treeChain->GetEntriesFast()  << endl;
+   L1UpgradeTree ntupleRawTree(treeChain);
+
+ 
+  std::map<unsigned int, unsigned int> lutMapEt;
+  std::map<unsigned int, unsigned int> lutMapEta;
+  std::map<unsigned int, unsigned int> lutMapNTT;
+  
+  for(UInt_t i = 1 ; i < N_OPTIONS ; ++i)
+    {
+      if(i == 0) continue ;
+      TString CurrentNameHisto = "LUT_Progression_";
+      ostringstream convert;
+      convert << i;
+      CurrentNameHisto += convert.str();
+      std::cout<<"Reading Option  :  "<<CurrentNameHisto<<"\n";
+      TH3F* current_Histo = (TH3F*)f_Isolation.Get(CurrentNameHisto.Data());
+      histosIsolation.insert(make_pair(CurrentNameHisto,current_Histo));
+    } 
+
+   float scaleFactor(1.0);
+   int optionsPtIsoMatrix[100][ET_MAX+1];
+ 
+   // Integrating the event pass distribution
+    for(int i=0;i< N_OPTIONS ; i++)
+    {
+        for(int j= 0 ; j < ET_MAX  ;j++)
+        {
+            optionsPtIsoMatrix[i][j]= 0 ;
+       }
+    }
+   UShort_t nEGs ;
+   std::vector<short>   egIEt;
+   std::vector<short>   egIEta;
+   std::vector<short>   egIsoEt;
+   std::vector<short>   egNTT;
+  
+   short in_compressedieta;
+   short in_compressedE;
+   short in_compressednTT;
+   short clippedEta;
+   short clippedEt;
+
+   int eTMax[N_OPTIONS];
+
+   //inTree->Print();
+   //inTree->Scan("nEGs");
+   
+   Long64_t nentries = ntupleRawTree.fChain->GetEntries();
+   Long64_t nbytes = 0, nb = 0;
+  
+    auto t_start = std::chrono::high_resolution_clock::now();
+    auto t_end = std::chrono::high_resolution_clock::now();
+
+   
+ cout << "Total Number of Events Available : " <<ntupleRawTree.fChain->GetEntries()  << endl;
+ cout << "Total Number of Events to process : " << nentries  << endl;
+ for (Long64_t jentry=0; jentry<nentries;jentry++) {
+
+      Long64_t ientry = ntupleRawTree.LoadTree(jentry);
+      if (ientry < 0) break;
+
+         nb = ntupleRawTree.fChain->GetEntry(jentry);   nbytes += nb;
+ 
+        if(jentry%5000 == 0) 
+        {
+             t_end = std::chrono::high_resolution_clock::now();
+             std::cout<<"Processing Entry in event loop : "<<jentry<<" / "<<nentries<<"  [ "<<100.0*jentry/nentries<<"  % ]  "
+             << " Elapsed time : "<< std::chrono::duration<double, std::milli>(t_end-t_start).count()/1000.0
+             <<"  Estimated time left : "<< std::chrono::duration<double, std::milli>(t_end-t_start).count()*( nentries - jentry)/jentry* 0.001
+             <<std::endl;
+        }
+
+         //std::cout<<"Entry #"<<jentry<<endl; 
+         //std::cout<<"\tnEGs = "<<ntupleRawTree.nEGs<<"\n";
+
+
+         // if(PU_per_LS.find(in_lumi)==PU_per_LS.end()) continue;
+         // Float_t weight = PU_per_LS[48]/PU_per_LS[in_lumi];
+         // if(weight<0.5) cout<<"lumi = "<<in_lumi<<", weight = "<<weight<<endl;
+         // if(weight>2) cout<<"lumi = "<<in_lumi<<", weight = "<<weight<<endl;
+
+         Float_t weight = 1.;
+         for(UInt_t i = 0 ; i < N_OPTIONS ; ++i)
+         {
+               eTMax[i] = -1; 
+         }
+
+         for(auto  eg_idx=0 ; eg_idx < ntupleRawTree.nEGs ; eg_idx++)
+         {
+             clippedEt  = ntupleRawTree.egIEt.at(eg_idx) < ET_MAX ? ntupleRawTree.egIEt.at(eg_idx) : ET_MAX ;
+            // std::cout<<"\t eg_idx "<<eg_idx<<" = "<<ntupleRawTree.egIEt.at(eg_idx)<<" [ "<<clippedEt<<" ] isoEt : "<<ntupleRawTree.egIsoEt.at(eg_idx)<<" \n";
+
+             if( ntupleRawTree.egIEta.at(eg_idx) > 31 )
+                clippedEta = 31 ;
+             else if ( ntupleRawTree.egIEta.at(eg_idx) < -31 )
+                clippedEta= -31 ;
+             else
+             {
+                clippedEta = ntupleRawTree.egIEta.at(eg_idx);
+             }
+
+             in_compressedieta =  lutMapEt[clippedEta]; 
+             in_compressedE    =  lutMapEta[clippedEt];
+             in_compressednTT  =  lutMapNTT[ntupleRawTree.egNTT.at(eg_idx)];
+
+             if(clippedEt > eTMax[0]) eTMax[0] = clippedEt;
+            
+
+             for(UInt_t i = 1 ; i < N_OPTIONS ; ++i)
+               {
+                 TString CurrentNameHisto = "LUT_Progression_";
+                 ostringstream convert;
+                 convert << i;
+                 CurrentNameHisto += convert.str();
+                 Int_t Cut_L1EG_Iso   = histosIsolation[CurrentNameHisto] ->GetBinContent(in_compressedieta+1,in_compressedE+1,in_compressednTT+1);
+                 
+                // std::cout<<" option : "<<i<<" : cut : "<<Cut_L1EG_Iso<<"\n";
+
+                 if ( ntupleRawTree.egIsoEt.at(eg_idx) > Cut_L1EG_Iso ) continue ;
+                 if(clippedEt > eTMax[i]) eTMax[i] = clippedEt;
+               } 
+          }
+          
+          for(UInt_t i=0 ;i < N_OPTIONS ; i++)
+          {
+               if( eTMax[i] > 0  )
+               {
+                //   std::cout<<" Pass for  "<<i<<" for "<<eTMax[i]<<"\n";
+                   optionsPtIsoMatrix[i][eTMax[i]] +=1;
+               }
+          }
+
+    }
+
+    // Integrating the event pass distribution
+    for(int i=0;i< N_OPTIONS ; i++)
+    {
+        for(int j= ET_MAX  ; j > 0  ;j--)
+        {
+            optionsPtIsoMatrix[i][j-1]+=optionsPtIsoMatrix[i][j] ;
+        }
+    }
+
+  // Filling the hist
+  ostringstream convert;
+  TFile *file =new TFile("L1EG_Rate_2018.root","RECREATE");
+  file->cd();
+
+  for(UInt_t i = 1 ; i < N_OPTIONS ; ++i )
+    {
+      TString CurrentNameHisto = "pass_LUT_Progression_";
+      convert.clear();  convert << i;
+      CurrentNameHisto += convert.str();
+      TH1F* pass_Histo = new TH1F(CurrentNameHisto, CurrentNameHisto , ET_MAX , 0.0 - 0.5 , ET_MAX -0.5 );
+      CurrentNameHisto = "rate_LUT_Progression_" + convert.str();
+      TH1F* rate_Histo = new TH1F(CurrentNameHisto, CurrentNameHisto ,ET_MAX , 0.0 - 0.5 , ET_MAX -0.5 );
+
+      for(int j=1;j<=ET_MAX;j++)
+        {
+            pass_Histo->SetBinContent(j,optionsPtIsoMatrix[i][j])  ;
+            rate_Histo->SetBinContent(j,optionsPtIsoMatrix[i][j]*scaleFactor) ;
+        }
+
+      pass_Histo->Write();
+      rate_Histo->Write();
+    } 
+
+    file->Write();
+    file->Close();
+
+}
+
+void tokenize(const std::string& str, std::vector<std::string>& tokens, const std::string& delimiters) {
+
+  // Skip delimiters at beginning.
+  std::string::size_type lastPos = str.find_first_not_of(delimiters, 0);
+
+  // Find first "non-delimiter".
+  std::string::size_type pos = str.find_first_of(delimiters, lastPos);
+
+  while (std::string::npos != pos || std::string::npos != lastPos)  {
+    // Found a token, add it to the vector.
+    tokens.push_back(str.substr(lastPos, pos - lastPos));
+
+    // Skip delimiters.  Note the "not_of"
+    lastPos = str.find_first_not_of(delimiters, pos);
+
+    // Find next "non-delimiter"
+    pos = str.find_first_of(delimiters, lastPos);
+  }
+}
 
 void readLUTTable(std::string& file_name, unsigned int& nbin,
 				     std::map<unsigned int, unsigned int>& lut_map){
@@ -62,198 +269,8 @@ void readLUTTable(std::string& file_name, unsigned int& nbin,
     }
     nbin += 1;
   }
-
   std::cout << " nbin " << nbin << std::endl;    
 }
 
-
-void rate_calculation()
-{
-
-
-    std::map<TString,TH3F*> histosIsolation;
-   TFile f_Isolation("/home/llr/cms/mperez/EGTrigger/CMSSW_9_2_10/src/EGTagAndProbe/EGTagAndProbe/test/Trees_meanparam/Iso_LUTs_Options_MC_92X_mean_newnTT.root","READ");
-   TString FileName_in = "/home/llr/cms/mperez/EGTrigger/CMSSW_9_2_10/src/EGTagAndProbe/EGTagAndProbe/test/Trees_meanparam/calibratedOutput_Ntuple_ZeroBias_Run305310_92X_mean_newnTT.root";
-  
-  std::map<unsigned int, unsigned int> lutMapEt;
-  std::map<unsigned int, unsigned int> lutMapEta;
-  std::map<unsigned int, unsigned int> lutMapNTT;
-  
-  for(UInt_t i = 0 ; i < N_OPTIONS ; ++i)
-    {
-      if(i == 0) continue ;
-
-      TString CurrentNameHisto = "LUT_Progression_";
-      ostringstream convert;
-      convert << i;
-      CurrentNameHisto += convert.str();
-      TH3F* current_Histo = (TH3F*)f_Isolation.Get(CurrentNameHisto.Data());
-      histosIsolation.insert(make_pair(CurrentNameHisto,current_Histo));
-    } 
-
-
-   int optionsPtIsoMatrix[100][ET_MAX+1];
-
-   TFile f_in(FileName_in.Data(),"READ");
-   TTree* inTree = (TTree*)f_in.Get("outTreeForCalibration");
- 
-   UShort_t nEGs ;
-   std::vector<short>   egIEt;
-   std::vector<short>   egIEta;
-   std::vector<short>   egIsoEt;
-   std::vector<short>   egNTT;
-  
-
-   inTree->SetBranchAddress("nEGs"   , &nEGs    );
-   inTree->SetBranchAddress("egIEt"  , &egIEt   );
-   inTree->SetBranchAddress("egIEta" , &egIEta  );
-   inTree->SetBranchAddress("egNTT"  , &egNTT   );
-   inTree->SetBranchAddress("isoEt"  , &egIsoEt );
-   
-   int eTMin[N_OPTIONS];
-
-   for(UInt_t i = 0 ; i < inTree->GetEntries() ; ++i)
-    {
-         inTree->GetEntry(i);
-         if(i%10000==0) cout<<"Entry #"<<i<<endl; 
-
-         // if(PU_per_LS.find(in_lumi)==PU_per_LS.end()) continue;
-         // Float_t weight = PU_per_LS[48]/PU_per_LS[in_lumi];
-         // if(weight<0.5) cout<<"lumi = "<<in_lumi<<", weight = "<<weight<<endl;
-         // if(weight>2) cout<<"lumi = "<<in_lumi<<", weight = "<<weight<<endl;
-
-         Float_t weight = 1.;
-
-         ++Denominator;
-         for(UInt_t i = 0 ; i < N_OPTIONS ; ++i)
-         {
-               eTMin = 10 +  ET_MAX; 
-         }
-
-         for(auto  eg_idx=0 ; eg_idx < nEGs ; eg_idx++)
-         {
-             clippedEt  = L1_EG_Et[eg_idx] < ET_MAX ? L1_EG_Et[eg_idx] : ET_MAX ;
-
-             if( egEta[eg_idx] > 31 )
-                clippedEta = 31 ;
-             else if ( egEta[eg_idx] < -31 )
-                clippedEta= -31 ;
-             else
-             {
-                clippedEta = egEta[eg_idx];
-             }
-
-             in_compressedieta =  lutMapEt[clippedEta]; 
-             in_compressedE    =  lutMapEta[clippedEt];
-             in_compressednTT  =  lutMapNTT[egNTT[eg_idx]];
-
-             for(UInt_t i = 1 ; i < N_OPTIONS ; ++i)
-               {
-                 TString CurrentNameHisto = "LUT_Progression_";
-                 ostringstream convert;
-                 convert << i;
-                 CurrentNameHisto += convert.str();
-                 Int_t Cut_L1EG_Iso   = histosIsolation[CurrentNameHisto] ->GetBinContent(in_compressedieta+1,in_compressedE+1,in_compressednTT+1);
-
-                 if ( egIsoEt[eg_idx] > Cut_L1EG_Iso ) continue ;
-                 if(clippedEt < eTMin[i]) eTMin[i] = clippedEt;
-               } 
-             
-          }
-          
-          for(UInt_t i=0 ;i < N_OPTIONS ; i++)
-          {
-               if( eTMin[i] <= ET_MAX )
-               {
-                   optionsPtIsoMatrix[i][eTMin[i]] +=1;
-               }
-          }
-
-    }
-
-    // Integrating the event pass distribution
-    for(int i=0;i< N_OPTIONS ; i++)
-    {
-        for(int j= ET_MAX-1 ; j >=0 ;j--)
-        {
-            optionsPtIsoMatrix[i][j]+=optionsPtIsoMatrix[i][j+1] ;
-        }
-    }
-
-  // Filling the hist
-  ostringstream convert;
-  TFile *file =new TFile("L1EG_Rate_2018.root","RECREATE");
-  file->cd();
-
-  for(UInt_t i = 1 ; i < 23 ; ++i)
-    {
-      TString CurrentNameHisto = "pass_LUT_Progression_";
-      convert.clea();  convert << i;
-      CurrentNameHisto += convert.str();
-      TH3F* pass_Histo = new TH3F(CurrentNameHisto.Data(), ET_MAX , 0.0 - 0.5 , ET_MAX -0.5 );
-      CurrentNameHisto = "rate_LUT_Progression_" + convert.c_str();
-      TH3F* rate_Histo = new TH3F(CurrentNameHisto.Data(), ET_MAX , 0.0 - 0.5 , ET_MAX -0.5 );
-
-      for(int j=1;j<=ET_MAX;j++)
-        {
-            pass_Histo->SetBinContent(j,optionsPtIsoMatrix[i][j])  ;
-            rate_Histo->SetBinContent(j,optionsPtIsoMatrix[i][j]*scaleFactor) ;
-        }
-
-      pass_Histo->Write();
-      rate_Histo->Write();
-    } 
-
-    file->Write();
-    file->Close();
-
-}
-
-
-
-TCanvas* CreateCanvas(TString CanvasName = "myPlot", bool LogY = false, bool Grid = true)
-{
-    TCanvas* c = new TCanvas(CanvasName.Data(),CanvasName.Data(),800,800);
-    c->SetLeftMargin(0.11);
-    if(Grid)
-        c->SetGrid();
-    if(LogY)
-        c->SetLogy();
-    return c;
-}
-
-void DrawPrelimLabel(TCanvas* c)
-{
-    c->cd();
-
-    TLatex tex;
-    tex.SetTextSize(0.03);
-    tex.DrawLatexNDC(0.11,0.91,"#scale[1.5]{CMS}");
-    tex.Draw("same");
-
-    return;
-}
-
-void DrawLumiLabel(TCanvas* c, TString Lumi = "35.9")
-{
-    c->cd();
-
-    TLatex tex;
-    tex.SetTextSize(0.035);
-    TString toDisplay = Lumi + " pb^{-1} (13 TeV)";
-    tex.DrawLatexNDC(0.66,0.91,toDisplay.Data());
-    tex.Draw("same");
-
-    return;
-}
-
-void SaveCanvas(TCanvas* c, TString PlotName = "myPlotName")
-{
-    c->cd();
-    c->SaveAs(PlotName + ".pdf");
-    c->SaveAs(PlotName + ".root");
-
-    return;
-}
 
 
