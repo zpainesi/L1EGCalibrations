189dfe89b5de97532c91b4b8cf513aa1e5c6f21d
aravind <aravindsugunan@gmail.com>
2023-04-03 17:13:47 +0530

    fit statbility updates

--- Isolation/test/python/makeFitDiagonosticPlots.py
+++ Isolation/test/python/makeFitDiagonosticPlots.py
@@ -10,35 +10,59 @@ def main():
     parser.add_argument('-t',"--tag", help="Tag for file",default='')
     parser.add_argument('-i',"--input", help="input file")
     parser.add_argument('-d',"--dest", help="output destination prefix ",default='./')
-    parser.add_argument('-e',"--eff", help="efficiency bin to make the plot for",default='10')
+    parser.add_argument("--effMin", help="min efficiency bin to make the plot for",default=1,type=int)
+    parser.add_argument("--effMax", help="max efficiency bin to make the plot for",default=100,type=int)
+    parser.add_argument('--ietaMin',help="Min value for iEta",default=0,type=int)
+    parser.add_argument('--ietMin' ,help="Min value for iEt ",default=0,type=int)
+    parser.add_argument('--ietaMax',help="Max value for iEta",default=15,type=int)
+    parser.add_argument('--ietMax' ,help="Max value for iEt ",default=15,type=int)
     args = parser.parse_args()
     
     inputWPFile = ROOT.TFile(args.input)
     tag=args.tag
     saveBase=args.dest+'/'
-    eff=int(args.eff)
+    if not os.path.exists(saveBase):
+        os.system('mkdir -p '+saveBase)
+
 
     x=np.arange(0,32,1)
-    
-    for eff in [eff]:
-        f,axes=plt.subplots(4,4,figsize=(20,20))
-        axes=np.ndarray.flatten(axes)
-        axid=0
-        pid=0
+    problematicProjectsionsAndFits={'vals':[] , 'fit' : [] ,'projections' : []}
+
+    f,axes=plt.subplots(4,4,figsize=(20,20))
+    axes=np.ndarray.flatten(axes)
+    axid=0
+    count=0
+    cMax= (args.effMax - args.effMin+1)*( args.ietaMax - args.ietaMin +1  )*(args.ietMax - args.ietMin +1)
+    badFitId=0
+    fid=0
+    pid=0
+    for eff in range(args.effMin,args.effMax+1):
         print("Processing Eff ",eff)
-        for ieta in range(16):
-            for et in range(16):
-                if axid%8==0:
-                    print(f"\t working on {ieta=} , {et=} ")
+        for ieta in range(args.ietaMin,args.ietaMax+1):
+            for et in range(args.ietMin,args.ietMax+1):
+                count+=1
+                if count%200==0:
+                    print(f"\t working on {eff=} {ieta=} , {et=}  {count} / {cMax}")
+                fid+=1    
                 ax=axes[axid]
                 proj=f'pz_{eff}_eta{ieta}_e{et}'
                 fName='fit_'+proj
-    
                 hist= inputWPFile.Get('Step1Histos/'+proj)
                 fit = inputWPFile.Get('Step1Histos/'+fName)
-    
-                a=fit.GetParameter(0)
-                b=fit.GetParameter(1)
+                try:
+                    a=fit.GetParameter(0)
+                    b=fit.GetParameter(1)
+                except:
+                    print(proj , hist)
+                    print(fName , fit)
+                    exit(1)
+
+                if b>0.01:
+                    continue
+                badFitId+=1
+                integral = hist.Integral()
+                problematicProjectsionsAndFits['vals'].append( np.array([ eff , ieta, et ]) )
+                print(f"\t\t{badFitId = } | Obtained a problematic fit at {eff=} {ieta=} {et=} {integral=} | {a = :.3} {b = :.2}"  )
                 chi2=fit.GetChisquare()
                 ndf=fit.GetNDF()
                 hep.histplot(hist,histtype='errorbar',ax=ax)
@@ -49,8 +73,7 @@ def main():
                 ax.plot(x,y,c='r')
                 axid+=1
                 if axid==16:
-                
-                    foutname=saveBase+f'{tag}DiagonosticPlotAtEff{eff}_part{pid}.png'
+                    foutname=saveBase+f'{tag}DiagonosticPlot_part{pid}.png'
                     print("Saving figure as  ",foutname)
                     f.savefig(foutname , bbox_inches='tight')
                     plt.close(f)
@@ -58,6 +81,30 @@ def main():
                     axes=np.ndarray.flatten(axes)
                     axid=0
                     pid+=1
+#                if badFitId > 40:
+#                    break    
+#            if badFitId > 40:
+#                break    
+    f.savefig(foutname , bbox_inches='tight')
+    axes=np.ndarray.flatten(axes)
+    plt.close(f)
+    
+    f=plt.figure(figsize=(15,12))
+    probalematicVals = np.array(problematicProjectsionsAndFits['vals'])
+    axe=plt.subplot(2,1,1)
+    axe.hist( probalematicVals[:,0] , bins=np.arange(-0.5,99.5 , 1.0 ))
+    axe.set_xlabel("Efficiency [ % ]")
+    axe.set_ylabel("n bad Fit")
+    axe=plt.subplot(2,2,3)
+    axe.hist( probalematicVals[:,1] , bins=np.arange(-0.5,args.ietaMax+0.5 , 1.0 ))
+    axe.set_xlabel("iEta")
+    axe.set_ylabel("n bad Fit")
+    axe=plt.subplot(2,2,4)
+    axe.hist( probalematicVals[:,2] , bins=np.arange(-0.5,args.ietMax +0.5 , 1.0 ))
+    axe.set_xlabel("iEt")
+    axe.set_ylabel("n bad Fit")
+    foutname=saveBase+f'{tag}DiagonosticPlot_summaryDistribution.png'
+    f.savefig(foutname , bbox_inches='tight')
 
 if __name__=='__main__':
     main()
